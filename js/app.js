2c2d95f7dd5414b42064c059bca65c9c5b05bd2a
elfiservice <armandojunior@Armandos-MacBook-Pro.local>
2017-05-22 10:46:56 -0300

    feat: Enemy and Player classes yours methods

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +16,14 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x < 505) {
+        this.x +=  (this.speed * dt );
+
+    } else {
+            this.x = -200;
+    }
+
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +34,41 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function (){
+    this.x = 205;
+    this.y = 400;
+    this.sprite = "images/char-boy.png";
+};
 
+Player.prototype.update = function(){
+
+}
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(keyCode){
+    console.log(keyCode);
+    if( keyCode === "up"){
+        this.y -= 81;
+
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [
+    new Enemy(0,50, 50),
+    new Enemy(-250,150, 100),
+    new Enemy(-100,225, 80),
+    new Enemy(-200,50, 150),
+    new Enemy(-550,150, 60),
+    new Enemy(-1000,225, 180)
+];
 
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
